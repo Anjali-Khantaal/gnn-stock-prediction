1718c78d97871f4b01b3393264f57fbc0cdc0b40
Anjali Khantaal <31400877+Anjali-Khantaal@users.noreply.github.com>
2024-09-23 19:33:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+TICKERS = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'META', 'NVDA', 'INTC', 'CSCO', 'IBM', 'ORCL']
+START_DATE = '2018-01-01'
+END_DATE = '2023-09-30'
+FEATURES = ['Returns', 'MA_5', 'MA_10', 'Sentiment']
